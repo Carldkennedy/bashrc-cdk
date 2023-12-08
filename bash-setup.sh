056214f6cb938118e446f6bad0a76e14870d54d8
Carl D Kennedy <116731613+Carldkennedy@users.noreply.github.com>
2023-12-08 14:36:22 +0000

    Update bash-setup.sh

--- bash-setup.sh
+++ bash-setup.sh
@@ -70,7 +70,7 @@ else
   echo "Hostname is unknown. Exiting script."
   exit 1
 fi
-tmux source ~/bashrc/tmux.conf
+tmux source ~/bashrc-cdk/tmux.conf
 
 # restore current working directory
 popd > /dev/null
