fee5ca0fad4b535609f9d3143cdad89bc31320e5
Carl D Kennedy <116731613+Carldkennedy@users.noreply.github.com>
2024-04-06 14:14:40 +0100

    Fix

--- custom_bindings.sh
+++ custom_bindings.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Custom key bindings
-bind -m vi-insert '^A':beginning-of-line'
-bind -m vi-insert '"jj":vi-movement-mode'
+bind -m vi-insert '^A':beginning-of-line
+bind -m vi-insert "jj":vi-movement-mode
