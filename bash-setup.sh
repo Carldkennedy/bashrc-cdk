e17e7192f3766cdaa5ada2e04d580244bd86b545
Carl D Kennedy <116731613+Carldkennedy@users.noreply.github.com>
2024-04-29 20:18:16 +0100

    Update bash-setup.sh

--- bash-setup.sh
+++ bash-setup.sh
@@ -1,65 +1,42 @@
 #!/bin/bash
 assign_hostname() {
-  if [[ "$HOSTNAME" == "*bessemer*" ]]; then
+  if [[ $HOSTNAME == *bessemer* ]]; then
     echo "bessemer"
-  elif [[ "$HOSTNAME" == "*stanage*" ]]; then
+  elif [[ $HOSTNAME == *stanage* ]]; then
     echo "stanage"
-  elif [[ "$HOSTNAME" == "*sharc*" ]]; then
+  elif [[ $HOSTNAME == *sharc* ]]; then
     echo "sharc"
   else
     echo "unknown"
   fi
 }
-# call function and assign hostname to variable
+
+# Call function and assign hostname to variable
 my_hostname=$(assign_hostname)
 
 echo "Hostname is $my_hostname"
-# save current working directory
+# Save and change to the working directory
 pushd . > /dev/null
-# Clone the repository to the user's home directory first.
-pushd . > /dev/null
-# Attempt to update the repository, and handle the outcome immediately
-cd ~/bashrc-cdk && if git pull > /dev/null 2>&1; then
+cd ~/bashrc-cdk
+
+# Attempt to update the repository, handling the outcome immediately
+if git pull > /dev/null 2>&1; then
   echo -e "##################################\nCustom bashrc updated from Github.\n##################################"
-    # check if hostname is known
+  # Check if hostname is known
   if [[ "$my_hostname" != "unknown" ]]; then
-    ## HOST Specific
-    # Check if the lines are already in the .bashrc file
-    if ! grep -Fxq ". ~/bashrc-cdk/bashrc_$my_hostname" ~/.bashrc
-    then
-      # If not, add them to the .bashrc file
-      echo -e "\n# Load custom bashrc for $my_hostname\n. ~/bashrc-cdk/bashrc_$my_hostname" >> ~/.bashrc
-    fi
-    if ! grep -Fxq ". ~/bashrc-cdk/bash_aliases_$my_hostname" ~/.bashrc
-    then
-      # If not, add them to the .bashrc file
-      echo -e "\n# Load custom bash aliases for $my_hostname\n. ~/bashrc-cdk/bash_aliases_$my_hostname" >> ~/.bashrc
-    fi
-  
+    # Host-specific configurations
+    grep -Fxq ". ~/bashrc-cdk/bashrc_$my_hostname" ~/.bashrc || echo -e "\n# Load custom bashrc for $my_hostname\n. ~/bashrc-cdk/bashrc_$my_hostname" >> ~/.bashrc
+    grep -Fxq ". ~/bashrc-cdk/bash_aliases_$my_hostname" ~/.bashrc || echo -e "\n# Load custom bash aliases for $my_hostname\n. ~/bashrc-cdk/bash_aliases_$my_hostname" >> ~/.bashrc
   fi
-  
-  ## Common
-  if ! grep -Fxq ". ~/bashrc-cdk/bashrc_common" ~/.bashrc
-  then
-    # If not, add them to the .bashrc file
-    echo -e "\n# Load custom common bashrc\n. ~/bashrc-cdk/bashrc_common" >> ~/.bashrc
-  fi
-  if ! grep -Fxq ". ~/bashrc-cdk/bash_aliases_common" ~/.bashrc
-  then
-    # If not, add them to the .bashrc file
-    echo -e "\n# Load custom common bash aliases\n. ~/bashrc-cdk/bash_aliases_common" >> ~/.bashrc
-  fi
-  if ! grep -Fq "~/bashrc-cdk/bash-setup.sh" ~/.bashrc
-  then
-    # If not, add them to the .bashrc file
-    echo -e "\n# Before enabling, check if git pull + SSH agent forwarding causes high CPU." >> ~/.bashrc
-    echo -e "# Load custom bashrc setup script\n#if [[ \$- == *i* ]]; then\n#    ~/bashrc-cdk/bash-setup.sh \n#fi" >> ~/.bashrc
-  fi
+
+  # Common configurations
+  grep -Fxq ". ~/bashrc-cdk/bashrc_common" ~/.bashrc || echo -e "\n# Load custom common bashrc\n. ~/bashrc-cdk/bashrc_common" >> ~/.bashrc
+  grep -Fxq ". ~/bashrc-cdk/bash_aliases_common" ~/.bashrc || echo -e "\n# Load custom common bash aliases\n. ~/bashrc-cdk/bash_aliases_common" >> ~/.bashrc
+  grep -Fq "~/bashrc-cdk/bash-setup.sh" ~/.bashrc || echo -e "\n# Before enabling, check if git pull + SSH agent forwarding causes high CPU.\n# Load custom bashrc setup script\n#if [[ \$- == *i* ]]; then\n#    ~/bashrc-cdk/bash-setup.sh \n#fi" >> ~/.bashrc
   tmux source ~/bashrc-cdk/tmux.conf
 else
   echo -e "################################\nCustom bashrc git update failed.\n################################"
 fi
 
-# restore current working directory
+# Restore the original working directory
 popd > /dev/null
-
